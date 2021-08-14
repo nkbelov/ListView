96e28217ee4a92535d06ce9dd3ce8b56ae208c2f
Nikita Belov <wtedst@gmail.com>
2021-08-14 22:21:00 +0200

    Initial Commit

---
+++ ListView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListView
+//
+//  Created by WTEDST on 14.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
