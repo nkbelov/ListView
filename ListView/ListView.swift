8a35c17ce7125521606106d8ca625d10ac6bb586
Nikita Belov <wtedst@gmail.com>
2021-08-14 23:08:06 +0200

    Basic setup

---
+++ ListView/ListView.swift
@@ -0,0 +1,13 @@
+//
+//  ListView.swift
+//  ListView
+//
+//  Created by WTEDST on 14.08.21.
+//
+
+import Foundation
+import UIKit
+
+final class ListView: UIScrollView {
+    
+}
--- ListView/ViewController.swift
+++ ListView/ViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let listView = ListView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(listView)
+        listView.backgroundColor = .systemTeal
+    }
+    
+    override func viewWillLayoutSubviews() {
+        listView.frame = view.bounds.inset(by: view.safeAreaInsets).insetBy(dx: 16, dy: 24)
     }
 
 
